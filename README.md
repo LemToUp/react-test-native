15cbda5400778eb80ddb306fe06ea40f3d3f205d
LemToUp <d.rumantsev@belitsoft.com>
2019-05-04 19:41:29 +0300

    Description added

---
+++ README.md
@@ -0,0 +1,11 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Before starting
+
+'cp -R hooks/* ../../node_modules/react-native/Libraries/Renderer'<br>
+npm install -g expo-cli<br>
+
+### `expo start`
+
+Runs app manager at http://localhost:19002/.<br>
+
--- components/WidgetPanel.js
+++ components/WidgetPanel.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import { Icon } from 'react-native-elements'
+import Filter from './Filter'
 
-function Widget(props) {
+function WidgetPanel(props) {
+    const [isFilterShow, toggleFilter] = useState(false);
+    const [selectedFilters, setSelectedFilters] = useState([]);
+    const [forceUpdateVariable, forceUpdate] = useState(false);
+
+    const onToggleFilter = (e) => {
+        console.log(e);
+        e.stopPropagation();
+        e.preventDefault();
+        toggleFilter(!isFilterShow);
+    };
+
+    const onGetFilters = (data) => { //Get data from filter
+        setSelectedFilters(data);
+        forceUpdate(!forceUpdateVariable);
+    };
+
+
     return (
         <View style={styles.widget}>
-            <Icon style={styles.icon} name='reorder'/>
+            <Icon
+                style={styles.icon}
+                name='reorder'
+                onPress={onToggleFilter}
+            />
+            <View style={styles.list}>
+                <FlatList
+                    data={selectedFilters}
+                    renderItem={({item}) => <Text>{item}</Text>}
+                />
+            </View>
+            <View>
+                <Filter
+                    isShow={isFilterShow}
+                    closeEvent={onToggleFilter}
+                    onGetData={onGetFilters}
+                    name={`filter_${props.number}`}
+                />
+            </View>
         </View>
     );
 }
@@ -14,14 +50,20 @@ const styles = StyleSheet.create({
     widget: {
         flex:1,
         backgroundColor: '#F7F7F7',
-        height: 100,
+        minHeight: 100,
         padding: '5%',
         flexDirection: 'row',
+        margin: '2%',
     },
     icon: {
         flex:0.3,
         alignItems: 'flex-start',
     },
+    list: {
+        flex:1,
+        backgroundColor: '#ffffff',
+        marginLeft: '5%',
+    },
 });
 
-export default Widget;
+export default WidgetPanel;
